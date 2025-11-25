3bce1201c556af93f327965526849c37fd6386b4
oleg-moseyko <moseyko28@gmail.com>
2025-11-25 12:18:21 +0100

    Add empty line for code clarity in chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,7 @@
 // api/chat.js
 import fetch from "node-fetch";
 
+
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).send("Method Not Allowed");
 
